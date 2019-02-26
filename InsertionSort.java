b481ee1bd331e84cca2ebf82cdc3706fc5543211
ajepstein <31577665+ajepstein@users.noreply.github.com>
2019-02-26 14:14:06 -0800

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,22 @@
+/**
+Adam Epstein
+3/3/19
+This programs sorts an array using Insertion Sort
+ */
+public class InsertionSort implements SortingAlgorithm 
+{
+    public void sort(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp)
+            {
+                arr[j+1] = arr[j];
+                --j; 
+            }
+            arr[j + 1] = temp;
+        }
+    }
+}
